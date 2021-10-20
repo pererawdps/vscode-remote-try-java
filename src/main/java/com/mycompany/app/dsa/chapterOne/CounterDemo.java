da04d858c1c97f312adc146b212bd9afe9202af3
pererawdps <pererawdps@gmail.com>
2021-10-20 02:24:12 +0000

    A demonstrate of the use of Counter instances

--- src/main/java/com/mycompany/app/dsa/chapterOne/CounterDemo.java
+++ src/main/java/com/mycompany/app/dsa/chapterOne/CounterDemo.java
@@ -11,6 +11,17 @@ class Counter{
 }
 public class CounterDemo {
     public static void main(String[] args) {
-        
+        Counter c; //declares a variable; no counter yet constructed
+        c = new Counter(); //constructs  a counter; assigns it reference to c
+        c.increment();//increases its value by one
+        c.increment(3); //increases its value by three more
+        int temp = c.getCount(); // will be 4
+        c.reset(); // value becomes 0
+        Counter d = new Counter(5);//declares and constructs a counter having 5
+        d.increment();//value becomes 6
+        Counter e = d; //assigns e to reference the same object as d
+        temp = e.getCount(); //will be 6 (as e and d reference the same counter)
+        e.increment(2); //value of e (also known as d) becomes 8
+
     }
 }
