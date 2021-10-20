3ede0a5cd5a89b3579733599e95db38aa25e3cc1
pererawdps <pererawdps@gmail.com>
2021-10-20 16:08:34 +0000

    A subclass of CreditCard

---
+++ src/main/java/com/mycompany/app/dsa/chapterOne/PredatoryCreditCard.java
@@ -0,0 +1,28 @@
+package com.mycompany.app.dsa.chapterOne;
+
+public class PredatoryCreditCard extends CreditCard {
+    //Additional instance variables
+    private double apr; //annual percentage rate
+
+    //Constructor for this class
+public PredatoryCreditCard(String cust, String bk, String acnt, int lim, double initialBal, double rate){
+    super(cust, bk, acnt, lim, initialBal); //initialize superclass attributes
+    apr = rate;
+}
+// A new method for assessing monthly interest charges
+public void processMonth(){
+    if(balance > 0 ){ //oly charge interest on a positive balance
+        double monthlyFactor = Math.pow(1+apr, 1.0/12); // compute monthly rate
+        balance *= monthlyFactor;
+
+    }
+}
+
+    //Overriding the charge method defined in the superclass
+    public boolean charge(double price){
+        boolean isSuccess = super.charge(price); //call inherited method
+        if(!isSuccess)
+        balance += 5; // assess a $5 penalty
+        return isSuccess;
+    }
+}
