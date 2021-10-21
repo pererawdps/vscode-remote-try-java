fa08400e6d7437b1a266af0c458d0c3a85126fa6
pererawdps <pererawdps@gmail.com>
2021-10-21 14:20:00 +0000

    Interface Person

---
+++ src/main/java/com/mycompany/app/dsa/chapterTwo/Person.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.dsa.chapterTwo;
+
+public interface Person {
+    public boolean equals(Person other); // Is this the same person?
+    public String getName(); //get this person's name
+    public int getAge(); //get this person's age
+    
+}
