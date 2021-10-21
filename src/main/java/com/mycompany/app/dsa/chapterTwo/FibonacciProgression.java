d0758cf1f1f05f1aa9a79aa210f2423fc6b52221
pererawdps <pererawdps@gmail.com>
2021-10-21 05:52:48 +0000

    Class for the Fibonacci progression

---
+++ src/main/java/com/mycompany/app/dsa/chapterTwo/FibonacciProgression.java
@@ -0,0 +1,21 @@
+package com.mycompany.app.dsa.chapterTwo;
+
+public class FibonacciProgression extends Progression{
+    protected long prev;
+
+    /** Constructs traditional Fibonacci starting 0,1,1,2,3,... */
+    public FibonacciProgression(){this(0,1);}
+
+    /**Constructs generalized Fibonacci, with the give first and second values. */
+    public FibonacciProgression(long first, long second){
+        super(first);
+        prev = second - first; //fictitious value preceding the first
+    }
+
+    /** Replaces (prev,current) with (current, current+prev)*/
+    protected void advance(){
+        long temp = prev;
+        prev = current;
+        current += temp;
+    }
+}
