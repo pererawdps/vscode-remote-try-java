7aad769f6a908333ff90633c20697c19367ad2e8
pererawdps <pererawdps@gmail.com>
2021-10-21 11:31:43 +0000

    Interface Transportable

---
+++ src/main/java/com/mycompany/app/dsa/chapterTwo/Transportable.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.dsa.chapterTwo;
+
+public interface Transportable {
+    /** Returns the weight in grams */
+    public int weight();
+    /** Returns whether the object is hazardous */
+    public boolean isHazardous();
+}
