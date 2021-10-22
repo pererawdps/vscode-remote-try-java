a6592302e48672f8da4e6380219e5a7be46b4820
pererawdps <pererawdps@gmail.com>
2021-10-22 04:34:49 +0000

    Generic pair of objects using classic style

---
+++ src/main/java/com/mycompany/app/dsa/chapterTwo/ObjectPair.java
@@ -0,0 +1,12 @@
+package com.mycompany.app.dsa.chapterTwo;
+
+public class ObjectPair {
+    Object first;
+    Object second;
+    public ObjectPair(Object a, Object b){
+        first = a;
+        second = b;
+    }
+    public Object getFirst(){return first;}
+    public Object getSecond(){return second;}
+}
